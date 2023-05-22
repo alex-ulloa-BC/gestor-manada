55c50cdc02a27c28afac32a936fb533eaf568ff9
Alex Ulloa <alex.ulloa@beautycountercontractor.com>
2023-05-21 20:06:49 -0500

    Initial Commit

---
+++ Gestor manada/Gestor_manadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestor_manadaApp.swift
+//  Gestor manada
+//
+//  Created by LAalex.ulloa on 21/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Gestor_manadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
